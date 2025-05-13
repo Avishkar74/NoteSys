f02964c056c3640686382d8e9c5780df438e288b
Avishkar74 <avishkarc600@gmail.com>
2025-05-13 17:51:09 +0530

    server created

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3000 
+
+app.use('/',express.static(path.join(__dirname, 'public')))
+
+app.use('/' , require('./routes/root'))
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use( (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
+
+app.listen(PORT,() => { console.log(`Server is running on port ${PORT}`)})
